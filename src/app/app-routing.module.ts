8d33982e63e69a5d7ae3ad6c73ee9c5c9ada0205
Uttam-Mahata <mahatauttam0408@gmail.com>
2024-11-05 13:57:59 +0530

    chore: Update router link for viewing chapters and topics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses/courses.component';
-import { SubjectsComponent } from './subjects/subjects.component';
-import { ModulesComponent } from './modules/modules.component';
-import { ChaptersComponent } from './chapters/chapters.component';
 import { TopicsComponent } from './topics/topics.component';
-import { SubtopicsComponent } from './subtopics/subtopics.component';
+
 import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
 import { SubjectsModulesComponent } from './subjects-modules/subjects-modules.component';
@@ -22,7 +19,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'courses', component: CoursesComponent },
   { path: 'courses/:course_id/subjects-modules', component: SubjectsModulesComponent },
-  // { path: 'courses/:course_id/subjects/:subject_id/modules', component: ModulesComponent },
+
   { path: 'courses/:course_id/subjects/:subject_id/modules/:module_id/chapters-topics', component: ChaptersTopicsComponent },
   { path: 'courses/:course_id/subjects/:subject_id/modules/:module_id/chapters/:chapter_id/topics', component: TopicsComponent },
   { path: 'courses/:course_id/subjects/:subject_id/modules/:module_id/chapters/:chapter_id/topics/:topic_id/subtopics-content', component: SubtopicsContentComponent },
