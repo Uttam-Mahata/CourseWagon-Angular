96e435f50ef378f4939dd4c1cc72eb4102317f17
Uttam-Mahata <uttambav@gmail.com>
2025-01-15 10:19:01 +0530

    feat: Add custom headers to login and register API requests

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -18,20 +18,34 @@ export class AuthService {
     this.isAuthenticatedSubject.next(!!localStorage.getItem('access_token'));
   }
 
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+  }
+
   login(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { email, password })
-      .pipe(
-        tap((response: any) => {
-          localStorage.setItem('access_token', response.access_token);
-          localStorage.setItem('refresh_token', response.refresh_token);
-          localStorage.setItem('user', JSON.stringify(response.user));
-          this.isAuthenticatedSubject.next(true);
-        })
-      );
+    return this.http.post(
+      `${this.apiUrl}/login`, 
+      { email, password },
+      { headers: this.getHeaders() }
+    ).pipe(
+      tap((response: any) => {
+        localStorage.setItem('access_token', response.access_token);
+        localStorage.setItem('refresh_token', response.refresh_token);
+        localStorage.setItem('user', JSON.stringify(response.user));
+        this.isAuthenticatedSubject.next(true);
+      })
+    );
   }
 
   register(userData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, userData);
+    return this.http.post(
+      `${this.apiUrl}/register`, 
+      userData,
+      { headers: this.getHeaders() }
+    );
   }
 
   logout(): void {
