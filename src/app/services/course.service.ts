3c409ecbe115da9d6f4893007dfd72b517282aab
Uttam Mahata <uttambav@gmail.com>
2025-06-27 06:18:32 +0530

    chore: update environment configuration and add Firebase dependency
    - Updated environment imports in multiple service files to use the correct environment configuration.
    - Changed API URLs in the environment.ts file to point to localhost for local development.
    - Added Firebase dependency to package.json.

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, throwError, of } from 'rxjs';
-import { environment } from '../../environments/environment.dev';
+import { environment } from '../../environments/environment';
 import { tap, catchError } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
 
